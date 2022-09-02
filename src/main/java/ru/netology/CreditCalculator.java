d3e0a29b46177948fdc487198f79a2a7a2198002
KristinaKac <kachalkovakristina@mailru>
2022-09-02 10:04:20 +0300

    Realization was add

--- src/main/java/ru/netology/CreditCalculator.java
+++ src/main/java/ru/netology/CreditCalculator.java
@@ -2,12 +2,12 @@ package ru.netology;
 
 public class CreditCalculator {
     public double monthlyPayment(int summaCredit, int periodCredit, double rateCredit){
-        return 0;
+        return (summaCredit * (rateCredit + (rateCredit / ((Math.pow((1 + rateCredit), periodCredit)) - 1))));
     }
     public double totalAmount(int summaCredit, int periodCredit, double rateCredit){
-        return 0;
+        return (summaCredit*(rateCredit + (rateCredit / ((Math.pow((1 + rateCredit), periodCredit)) - 1))))* periodCredit;
     }
     public double overPayment(int summaCredit, int periodCredit, double rateCredit){
-        return 0;
+        return (summaCredit*(rateCredit + (rateCredit / ((Math.pow((1 + rateCredit), periodCredit)) - 1))))* periodCredit - summaCredit;
     }
 }
